ed50203461df8f2e912274d1f54a4d8e479a8a2d
lenny <l.cartier@useene.net>
2013-12-31 14:16:30 +0100

    First commit

---
+++ plg_annotator/README.md
@@ -0,0 +1,14 @@
+plg_annotator
+=============
+
+Annotator.js joomla plugin
+
+_Usage_
+
+- Install the plugin with Joomla plugin installer.
+- Go to Extensions/Plugin manager.
+- Search for "annotate" plugin, then go edit the settings.
+- Set the div it to annotate
+- Choose available options (local/remote files, minified or not)
+- Don't forget to activate !
+- Browse an article with content inside a `<div id="divtoannotate">`
---
+++ plg_annotator/annotator.php
@@ -0,0 +1,35 @@
+<?php
+
+// no direct access
+defined('_JEXEC') or die;
+
+jimport('joomla.plugin.plugin');
+
+class plgContentAnnotator extends JPlugin
+{
+    public function onContentAfterTitle($context, &$row, &$params, $limitstart)
+	        {
+
+	    $document = JFactory::getDocument();
+
+	    $document->addScript('/media/jui/js/jquery.js');
+
+		// build local/remote url
+		if ( $this->params->get('annotate-source') == 'local') { $source = "/plugins/content/annotator/annotator/v"; } else { $source = "http://assets.annotateit.org/annotator/v"; }
+		
+		// call js & css		
+	    if ( $this->params->get('minified-js') == '1' ) { 
+	    $document->addScript($source.$this->params->get('annotate-version') ."/annotator-full.min.js"); 
+	    	} else { $document->addScript($source.$this->params->get('annotate-version') ."/annotator-full.js"); }
+
+	    if ( $this->params->get('minified-css') == '1' ) {	
+	    $document->addStyleSheet($source.$this->params->get('annotate-version') ."/annotator.min.css");
+	    	} else { $document->addStyleSheet($source.$this->params->get('annotate-version') ."/annotator.css"); }
+	    
+		// call annotator	
+		$javascript = "<div><script type=\"text/javascript\"> jQuery(function ($) { $('#". $this->params->get('annotate-class') ."').annotator().annotator('setupPlugins'); }); </script></div>";
+		return $javascript;
+		
+		}
+}
+?>
