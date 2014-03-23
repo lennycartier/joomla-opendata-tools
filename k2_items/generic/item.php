bd3a06709cb0aad43478392a7bf55db97c1a0dad
Lenny Cartier <l.cartier@datanotes.org>
2014-03-23 23:17:32 +0100

    Add modifications author

--- k2_items/generic/item.php
+++ k2_items/generic/item.php
@@ -5,6 +5,7 @@
  * @author		JoomlaWorks http://www.joomlaworks.net
  * @copyright	Copyright (c) 2006 - 2014 JoomlaWorks Ltd. All rights reserved.
  * @license		GNU/GPL license: http://www.gnu.org/copyleft/gpl.html
+ * modified by Lenny Cartier 
  */
 
 // no direct access
